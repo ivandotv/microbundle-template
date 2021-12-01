28b513167755b585f21c38dc595438b58ce0e009
ivandotv <390700+ivandotv@users.noreply.github.com>
2021-12-01 17:30:39 +0100

    cleanup index file

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,3 @@ export function demo(): void {
   console.log(__VERSION__)
 }
 demo()
-
-// @ts-expect-error - test code removal
-function not_used(): void {
-  console.log('this should not be present after minification')
-}
