19fc5745d9d5ac72193fa67e2a9e73d20ef7ad95
ivandotv <390700+ivandotv@users.noreply.github.com>
2021-12-09 23:48:26 +0100

    generate example docs

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
+/**
+ * Demo function for template repository
+ *
+ * @remarks
+ * Read more about TSDoc at: {@link https://github.com/microsoft/tsdoc}
+ *
+ * @param name - Demo name
+ * @returns Nothing!
+ *
+ * @beta
+ */
 export function demo(): void {
   if (__DEV__) {
-    console.log('this should show only in development build')
+    console.log('this should only log in development build')
   }
   console.log(__VERSION__)
 }
