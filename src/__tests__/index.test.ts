a7985f424341650da7bb5dbbfac9898d434590c9
Ivan Vlatković <390700+ivandotv@users.noreply.github.com>
2024-09-29 13:56:34 +0200

    remove failing assertion

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -4,7 +4,7 @@ import { demo } from "../index"
 describe("Test", () => {
   test("Demo Test", () => {
     expect(demo).toBeDefined()
-    expect(true).toBe(false)
+    // expect(true).toBe(false)
   })
   test.todo("Write more tests!")
 })
