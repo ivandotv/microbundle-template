f9b9e662ab913dcc4095639eab273ff79f165b0e
Ivan Vlatković <390700+ivandotv@users.noreply.github.com>
2024-09-29 13:57:54 +0200

    add line test

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -5,6 +5,7 @@ describe("Test", () => {
   test("Demo Test", () => {
     expect(demo).toBeDefined()
     // expect(true).toBe(false)
+    // expect(true).toBe(false)
   })
   test.todo("Write more tests!")
 })
