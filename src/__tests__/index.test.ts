0107ac04bb835ab329b69d85df6526ed8c0a0fe0
Ivan Vlatković <390700+ivandotv@users.noreply.github.com>
2024-09-29 12:05:06 +0200

    failed test

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,8 +1,10 @@
 import { describe, expect, test } from "vitest"
 import { demo } from "../index"
+
 describe("Test", () => {
   test("Demo Test", () => {
     expect(demo).toBeDefined()
+    expect(true).toBe(false)
   })
   test.todo("Write more tests!")
 })
