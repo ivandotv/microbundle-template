5059cc6341db3aceba7d967119970562856c4ee1
Ivan Vlatković <390700+ivandotv@users.noreply.github.com>
2024-09-29 13:59:52 +0200

    Refactor test assertions in index.test.ts

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -4,8 +4,6 @@ import { demo } from "../index"
 describe("Test", () => {
   test("Demo Test", () => {
     expect(demo).toBeDefined()
-    // expect(true).toBe(false)
-    // expect(true).toBe(false)
   })
   test.todo("Write more tests!")
 })
